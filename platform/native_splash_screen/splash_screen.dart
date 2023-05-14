429cc21572d637487c78da96df86d670adeab368
Vladislav Shamrin <shamrin20077@gmail.com>
2023-05-14 11:11:08 +0300

    Added 'native_splash_screen' extension

---
+++ platform/native_splash_screen/splash_screen.dart
@@ -0,0 +1,16 @@
+// The file contains class for splash screen
+
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:TODO/common/app_constants/app_settings.dart';
+
+void runSplashScreen() async {
+  // Run and duration the native splash screen
+
+  FlutterNativeSplash.preserve(
+      widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
+
+  await Future<dynamic>.delayed(AppSettings.splashScreenDuration);
+
+  FlutterNativeSplash.remove();
+}
